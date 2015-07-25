bc1f5971fd1ae2a93dcbe2bb3b5d26166b072fcc
Kristian Dupont <kristian@kristiandupont.com>
2015-07-25 15:47:44 +0200

    More refactoring

--- browserSystem.js
+++ browserSystem.js
@@ -10,24 +10,28 @@ BrowserSystem.prototype.addBrowser = function (socket) {
   this.sockets[socket.id] = socket;
 }
 
+function gettingBrowserEvent(redisCx, event) {
+  return new Promise(function (resolve, reject) {
+    if (event.name === "message-list") {
+      redisCx.lrange("messages:" + event.roomId, 0, -1, function (err, data) {
+        if (err) {
+          reject();
+        } else {
+          var e = _.extend({}, event, { messages: _.map(data, JSON.parse) });
+          resolve(e);
+        }
+      });
+    } else {
+      return Promise.resolve(event);
+    }
+  });
+}
+
 BrowserSystem.prototype.processing = function (events) {
   var promises = _.map(events, function (event) {
     switch(event.target) {
       case 'browser':
-        return new Promise(function (resolve, reject) {
-          if (event.name === "message-list") {
-            this.redisCx.lrange("messages:" + event.roomId, 0, -1, function (err, data) {
-              if (err) {
-                reject();
-              } else {
-                var e = _.extend({}, event, { messages: _.map(data, JSON.parse) });
-                resolve(e);
-              }
-            }.bind(this));
-          } else {
-            resolve(event);
-          }
-        }.bind(this)).then(function (e) {
+        gettingBrowserEvent(this.redisCx, event).then(function (e) {
           _.each(event.clients, function (client) {
             this.sockets[client].emit(e.name, e);
           }.bind(this));
